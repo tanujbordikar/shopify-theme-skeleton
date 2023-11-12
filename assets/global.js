3bf34197041693631fa0f52117d33bb280c8d29f
Mihir Dave <78968174+mihir-bombay-studio@users.noreply.github.com>
2023-11-12 17:12:43 +0530

    optimise announcement bar

--- assets/global.js
+++ assets/global.js
@@ -9,44 +9,68 @@ const abc = Vue.createApp({
 
 class AnnouncementSlider {
   constructor(selector) {
+    // If already initialized, skip creating a new instance
+    if (AnnouncementSlider.instances[selector]) {
+      return;
+    }
+
     this.sliderContainer = document.querySelector(selector);
+    if (!this.sliderContainer) {
+      throw new Error(`No element found for selector: ${selector}`);
+    }
+
     this.slides = this.sliderContainer.querySelectorAll('.announcement__item');
     this.currentSlide = 0;
     this.duration = this.getDuration();
-    this.initSlider();
+
+    // Save the instance to prevent re-initialization
+    AnnouncementSlider.instances[selector] = this;
+
+    if (this.slides.length > 1) {
+      this.initSlider();
+    }
   }
 
+  static instances = {};
+
   getDuration() {
-    // Get the duration from the data object
-    // Convert the duration from seconds to milliseconds
-    // Default to 4000ms (4 seconds) if the duration is not set or invalid
-    const duration = this.sliderContainer.dataset.duration;
-    return duration ? parseInt(duration, 10) * 1000 : 4000;
+    // Simplified duration retrieval with a fallback
+    const duration = parseInt(this.sliderContainer.dataset.duration, 10);
+    return isNaN(duration) ? 4000 : duration * 1000;
   }
 
   initSlider() {
-    if (this.slides.length > 1) {
-      setInterval(() => this.goToNextSlide(), this.duration);
-    }
+    // Using arrow function for 'this' context preservation
+    this.intervalId = setInterval(() => this.goToNextSlide(), this.duration);
   }
 
   goToNextSlide() {
     const nextSlide = (this.currentSlide + 1) % this.slides.length;
-    this.animateSlideInOut(this.currentSlide,nextSlide);
+    this.animateSlideInOut(this.currentSlide, nextSlide);
   }
 
   animateSlideInOut(currentSlide, nextSlide) {
     this.slides[nextSlide].classList.add('slide-in');
     this.slides[currentSlide].classList.add('slide-out-left');
-    
+
     setTimeout(() => {
-      this.slides[currentSlide].classList.remove('slide-out-left', 'slide-in');
-      this.currentSlide = nextSlide; // Update currentSlide after animation is complete
-    }, 1000); // Match with CSS animation duration
+      this.slides[currentSlide].classList.remove('slide-in', 'slide-out-left');
+      this.currentSlide = nextSlide; // Update currentSlide
+    }, 1000); // Assuming the animation duration is 1000ms
   }
 }
 
-// Initialization
+// Initialization on DOM ready
 document.addEventListener('DOMContentLoaded', function () {
-  new AnnouncementSlider('.announcement__wrapper');
+    new AnnouncementSlider('.announcement__wrapper');
 });
+
+// Shopify design mode
+if (Shopify.designMode) {
+  document.addEventListener('shopify:section:load', (event) => {
+    console.log(event.detail);
+    const sectionId = event.detail.sectionId;
+    const selector = `#shopify-section-${sectionId} .announcement__wrapper`;
+    new AnnouncementSlider(selector);
+  });
+}
