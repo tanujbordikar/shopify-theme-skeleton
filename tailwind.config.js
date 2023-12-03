5910c77acec8069aa97bd3a2491dbadbbe8e2df1
anishDalvi <anishdalvi2001@gmail.com>
2023-12-04 00:25:13 +0530

    keyframes not working from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-    './layout/*.liquid', 
-    './sections/*.liquid', 
-    './snippets/*.liquid',
-    './templates/customers/*.liquid',
-    './templates/*.liquid',
-    './sections/*.json',
-    './templates/*.json'
+  content: [
+    "./layout/*.liquid",
+    "./sections/*.liquid",
+    "./snippets/*.liquid",
+    "./templates/customers/*.liquid",
+    "./templates/*.liquid",
+    "./sections/*.json",
+    "./templates/*.json",
   ],
-    theme: {
+  theme: {
+    extend: {
+      keyframes: {
+        slide: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        slide: "slide 3s linear infinite",
+      },
     },
-  }
+  },
+};
