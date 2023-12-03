f9a364b51d38b2a606a429ce7574f9d32c0d6883
anishDalvi <anishdalvi2001@gmail.com>
2023-12-04 00:28:10 +0530

    animation added to Logo Slide

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./layout/*.liquid",
-    "./sections/*.liquid",
-    "./snippets/*.liquid",
-    "./templates/customers/*.liquid",
-    "./templates/*.liquid",
-    "./sections/*.json",
-    "./templates/*.json",
+    content: [
+    './layout/*.liquid', 
+    './sections/*.liquid', 
+    './snippets/*.liquid',
+    './templates/customers/*.liquid',
+    './templates/*.liquid',
+    './sections/*.json',
+    './templates/*.json'
   ],
-  theme: {
-    extend: {
-      keyframes: {
-        slide: {
-          from: { transform: "translateX(0)" },
-          to: { transform: "translateX(-100%)" },
-        },
-      },
-      animation: {
-        slide: "slide 3s linear infinite",
-      },
+    theme: {
     },
-  },
-};
+  }
